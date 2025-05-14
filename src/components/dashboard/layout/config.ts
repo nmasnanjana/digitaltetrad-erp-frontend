cf440185badadf41332ba70a787cd06ff34840b0
Anjana Narasinghe <nmasnanjana.123@gmail.com>
2025-05-14 11:06:48 +0530

    add expense sub menue

--- src/components/dashboard/layout/config.ts
+++ src/components/dashboard/layout/config.ts
@@ -3,7 +3,11 @@ import { paths } from '@/paths';
 import {Money} from "@phosphor-icons/react/dist/ssr/Money";
 
 export const navItems = [
-  { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
+  { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie',
+    items: [
+      {key: 'expenseDashboard', title: 'Expense Overview', href: paths.dashboard.expenseDashboard, icon: 'chart-pie'},
+    ]
+  },
   //{ key: 'integrations', title: 'Integrations', href: paths.dashboard.integrations, icon: 'plugs-connected' },
   //{ key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
   { key: 'user', title: 'User Management', href: paths.dashboard.user, icon: 'user' },
--- src/paths.ts
+++ src/paths.ts
@@ -15,6 +15,7 @@ export const paths = {
     customer: '/dashboard/customer',
     job: '/dashboard/job',
     expense: '/dashboard/expense',
+    expenseDashboard: '/dashboard/expenseDashboard',
   },
   errors: { notFound: '/errors/404' },
 } as const;
